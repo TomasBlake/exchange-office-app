d524c5619ff180faa14d7e2763aff89b510edffa
tomasblake <lonsky.to@gmail.com>
2023-10-14 15:14:02 +0200

    configure api

--- exchange-office-app-backend/src/index.ts
+++ exchange-office-app-backend/src/index.ts
@@ -9,24 +9,20 @@ const PORT = process.env.PORT || 8000;
 
 app.get("/exchange-rates", async (req: Request, res: Response) => {
   try {
-    // URL, na kterou budeme posílat požadavek
+   
     const url = process.env.CNB_API || "";
 
-    // Odeslání HTTP požadavku a čekání na odpověď
     const response = await fetch(url);
 
-    // Kontrola, zda je odpověď v pořádku
     if (!response.ok) {
       throw new Error("Network response was not ok: " + response.statusText);
     }
 
-    // Načtení dat jako textu
     const data = await response.text();
 
-    // Odeslání dat jako odpověď na původní požadavek
     res.send(data);
   } catch (error) {
-    // Odeslání chyby, pokud něco selže
+
     console.error(error);
     res.status(500).send("Internal Server Error");
   }
